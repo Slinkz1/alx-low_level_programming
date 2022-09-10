48b6126c51191ece7e7576f0007a1a30748022b4
Samuel Egwurugwu <sammylinks85@gmail.com>
2022-09-10 14:38:24 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,12 @@
 #include<stdio.h>
 #include<string.h>
-
 #include<unistd.h>
 /**
- *main -main block
+ * main -main block
  *Return:1
  */
 int main(void)
 {
-
-	 int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-	  write(2,
-			  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-			  s);
-	  return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
+return (1);
 }
-
-
