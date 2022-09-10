995dd225ceeb03469543df02cee4a7cd3f14f4ae
Samuel Egwurugwu <sammylinks85@gmail.com>
2022-09-10 05:42:25 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /**
- * main block-main
+ * main - Entry point
  * return:0
  */
-	int main(void)
-	{ 		
-		puts("\"programming is like building a multilingual puzzle");
-		return(0);
-	}
-
+int main(void)
+{ 		
+	puts("\"programming is like building a multilingual puzzle");
+	return(0);
+}
