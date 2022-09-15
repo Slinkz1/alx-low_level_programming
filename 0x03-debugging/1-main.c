3c2f63990a39de87fb09a6910b8e7e008a0c26ca
Samuel Egwurugwu <sammylinks85@gmail.com>
2022-09-15 16:29:48 -0700

    our commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
-	int i;
+int i;
 
-	printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
-	i = 0;
+i = 0;
 
 /*
  * while (i < 10)
