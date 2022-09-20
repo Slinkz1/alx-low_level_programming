a5b195b16c0f1078fa122f8d1531bfec2d7ca112
Samuel Egwurugwu <sammylinks85@gmail.com>
2022-09-20 13:57:35 -0700

    commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,22 +6,23 @@
  */
 int _atoi(char *s)
 {
-	int counter = 0;
-	int i = 0;
-	unsigned int result = 0;
-	int sign = 0;
+int counter = 0;
+int i = 0;
+unsigned int result = 0;
+int sign = 0;
 
-	while (s[counter] != '\0')
-	{
-		if (s[counter] == '-')
-			sign++;
-		if (s[counter] >= 48 && s[counter] <= 57)
-		{
-			i++;
-			result = result * 10 + s[counter] - '0';
-		}
-		if (i > 0 && (s[counter] < 48 || s[counter] > 57))
-			break;
+while (s[counter] != '\0')
+{
+if (s[counter] == '-')
+sign++;
+if (s[counter] >= 48 && s[counter] <= 57)
+{
+i++;
+result = result * 10 + s[counter] - '0';
+}
+if (i > 0 && (s[counter] < 48 || s[counter] > 57))
+break;
+
 		counter++;
 
 		if (i == 0)
